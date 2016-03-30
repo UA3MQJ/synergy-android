af14ec0e4752842ed87df510dbf3c7dc3b5f1ac1
Alex <ua3mqj@gmail.com>
2016-03-31 01:24:04 +0300

    initial

---
+++ synergy-android/www/script.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('synergy', ['ngResource','ui.bootstrap', 'ui.router', 'ui.navbar']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/login");
+
+    // Now set up the states
+    $stateProvider
+        .state('login', {
+            url: "/login",
+            templateUrl: "login.html"
+        })
+        .state('account', {
+            url: "/account",
+            templateUrl: "account.html"
+        })
+});
+
+app.controller('MainCtrl', function($scope) {
+    $scope.name = 'World';
+});
